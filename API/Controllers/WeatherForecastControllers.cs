06a3f2ddfc49ac03f39f86588526813c0f20369d
Joseph Garrison <garrisj2@gmatc.matc.edu>
2024-09-15 16:57:56 -0500

    Completed Activity 3

--- API/Controllers/WeatherForecastControllers.cs
+++ API/Controllers/WeatherForecastControllers.cs
@@ -1,5 +1,6 @@
 using Domain;
 using Microsoft.AspNetCore.Mvc;
+using Persistence;
 
 namespace API.Controllers;
 
@@ -8,26 +9,51 @@ namespace API.Controllers;
 
 public class WeatherForecastController : ControllerBase
 {
-private static readonly string[] Summaries =
-[
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-];
-private readonly ILogger<WeatherForecastController> _logger;
+    private static readonly string[] Summaries =
+    [
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    ];
+    private readonly ILogger<WeatherForecastController> _logger;
+    private readonly DataContext _context;
 
-public WeatherForecastController(ILogger<WeatherForecastController> logger)
-{
-    _logger = logger;
-}
+    public WeatherForecastController(ILogger<WeatherForecastController> logger, DataContext context)
+    {
+        _logger = logger;
+        _context = context;
+    }
 
-[HttpGet(Name="GetWeatherForecast")]
-public IEnumerable<WeatherForecast> Get()
-{
-    return Enumerable.Range(1,5).Select(index => new WeatherForecast
-{
-    Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-    TemperatureC = Random.Shared.Next(-20, 55),
-    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-})
-.ToArray();
-}
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+    .ToArray();
+    }
+
+    [HttpPost]
+    public ActionResult<WeatherForecast> Create() {
+        Console.WriteLine($"Database path: {_context.DbPath}");
+        Console.WriteLine("Insert a new WeatherForecast");
+ 
+        var forecast = new WeatherForecast()
+        {
+            Date = new DateOnly(),
+            TemperatureC = 75,
+            Summary = "Warm"
+        };
+ 
+        _context.WeatherForecasts.Add(forecast);
+        var success = _context.SaveChanges() > 0;
+ 
+        if (success)
+        {
+            return forecast;
+        }
+ 
+        throw new Exception("Error creating WeatherForecast");
+    }
 }
