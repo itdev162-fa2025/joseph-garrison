6fd94c95b43f9fd5729beadc34f0babeb4b66989
Joseph Garrison <garrisj2@gmatc.matc.edu>
2024-10-06 15:26:04 -0500

    added Post entity and Post entity controller

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Domain;
 using Microsoft.EntityFrameworkCore;
  
@@ -6,6 +10,7 @@ namespace Persistence
     public class DataContext: DbContext
     {
         public DbSet<WeatherForecast> WeatherForecasts { get; set; }
+        public DbSet<Post> Posts {get; set;}
         public string DbPath { get; }
  
         public DataContext()
