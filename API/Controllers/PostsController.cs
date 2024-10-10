279199ea1f9803321358e2808db9e0ac47f09333
Joseph Garrison <garrisj2@gmatc.matc.edu>
2024-10-10 13:18:16 -0500

    2nd commit activity 6

--- API/Controllers/PostsController.cs
+++ API/Controllers/PostsController.cs
@@ -58,7 +58,33 @@ namespace API.Controllers
 
             throw new Exception("Error creating post");
         }
-    
+
+        ///<summary>
+        ///PUT api/post
+        ///</summary>
+        ///<param name="request">JSON request containing one or more updated post fields</param>
+        ///<return>An updated post</returns>
+        [HttpPut(Name = "Update")]
+        public ActionResult<Post> Update([FromBody]Post request)
+        {
+            var post = _context.Posts.Find(request.Id);
+            if (post == null)
+            {
+                throw new Exception("Could not find post");
+            }
+
+            post.Title = request.Title != null ? request.Title : post.Title;
+            post.Body = request.Body != null ? request.Body : post.Body;
+            post.Date = request.Date != DateTime.MinValue ? request.Date : post.Date;
+
+            var success = _context.SaveChanges() > 0;
+            if (success)
+            {
+                return Ok(post);
+            }
+
+            throw new Exception("Error updating post");
+        }
     }
 
 }
