2f29ca29a8d72b98e3a20337a9d017386ec33e38
Joseph Garrison <garrisj2@gmatc.matc.edu>
2024-10-10 12:14:38 -0500

    1st commit actiivty 6

--- API/Controllers/PostsController.cs
+++ API/Controllers/PostsController.cs
@@ -21,5 +21,44 @@ namespace API.Controllers
         {
             return this._context.Posts.ToList();
         }
+        [HttpGet("{id}", Name = "GetById")]
+        public ActionResult<Post> GetById(Guid id)
+        {
+            var post = this._context.Posts.Find(id);
+            if (post is null)
+            {
+                return NotFound();
+            }
+            return Ok(post);
+        }
+
+        ///<summary>
+        ///POST api/post
+        ///</summary>
+        ///<param name= "request">JSON request containing post fields</param>
+        ///<return>A new post</returns>
+        [HttpPost(Name = "Create")]
+        public ActionResult<Post> Create([FromBody]Post request)
+        {
+            var post = new Post
+            {
+                Id = request.Id,
+                Title = request.Title,
+                Body = request.Body,
+                Date = request.Date
+            };
+
+            _context.Posts.Add(post);
+            var success = _context.SaveChanges() > 0;
+
+            if (success)
+            {
+                return Ok(post);
+            }
+
+            throw new Exception("Error creating post");
+        }
+    
     }
+
 }
